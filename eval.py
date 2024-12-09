82fdc90dbb2073e4c338a8e54edce3d27f773bc4
LoganJu <loganju2000@gmail.com>
2024-12-09 00:27:26 -0500

    add HR

---
+++ eval.py
@@ -0,0 +1,14 @@
+from typing import List, Dict
+from data import Data
+
+
+class Evaluation:
+    def __init__(self, data: Data):
+        self.data = data
+
+    def evaluate_hit_rate(self, user_recommendations: Dict[int, List[int]]) -> float:
+        user_ids = list(user_recommendations.keys())
+        recommendations = list(user_recommendations.values())
+
+        hit_rate = self.data.hit_rate(user_ids, recommendations)
+        return hit_rate
