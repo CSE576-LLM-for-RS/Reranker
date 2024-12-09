01caf8af2d69078197911cdc46598ed5cde16c11
LoganJu <loganju2000@gmail.com>
2024-12-09 00:28:00 -0500

    include N

--- eval.py
+++ eval.py
@@ -6,9 +6,11 @@ class Evaluation:
     def __init__(self, data: Data):
         self.data = data
 
-    def evaluate_hit_rate(self, user_recommendations: Dict[int, List[int]]) -> float:
+    def evaluate_hit_rate(
+        self, user_recommendations: Dict[int, List[int]], N: int
+    ) -> float:
         user_ids = list(user_recommendations.keys())
         recommendations = list(user_recommendations.values())
 
-        hit_rate = self.data.hit_rate(user_ids, recommendations)
+        hit_rate = self.data.hit_rate(user_ids, recommendations[:N])
         return hit_rate
